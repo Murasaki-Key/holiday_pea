eaf28428d977f82ee5fc615a55d8aa61a0cc067a
id <id@AQ-0117>
2023-07-06 16:08:55 +0900

    コントローラ変更

--- src/main/java/com/example/demo/controller/MyPageController.java
+++ src/main/java/com/example/demo/controller/MyPageController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.entity.TimeTable;
 import com.example.demo.model.User;
+import com.example.demo.repository.TimeTableListRepository;
 import com.example.demo.repository.TimeTableRepository;
 
 import jakarta.servlet.http.HttpSession;
@@ -26,10 +27,15 @@ public class MyPageController {
 	@Autowired
 	TimeTableRepository timetableRepository;
 	
+	@Autowired
+	TimeTableListRepository timetablelistRepository;
+	
 	@PostMapping("/myplan")
 	public String myplan(Model m) {
 		String username = users.getName();
 		m.addAttribute("username", username);
+		
+		timetablelistRepository.deleteAll();
 		
 		List <TimeTable> timetable = timetableRepository.decidePlanid(username);
 		m.addAttribute("timetable",timetable);
